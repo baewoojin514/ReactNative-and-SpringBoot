aed0824689603a26c07eebd3623cdbc9dd92875b
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-27 14:39:24 +0900

    button is created and click action handler is updated for button

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>react Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const state = {
+  counter: 0,
+}
+
+export default class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  render(){
+    const clickHandler = () => {
+      state.counter ++;
+      console.log(state.counter)
+    }
+    return (
+      <View style={styles.container}>
+        <Text>{state.counter}</Text>
+        <Button
+          title={"Click me!"}
+          onPress={clickHandler}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +38,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textBig: {
+    fontSize: 32,
+    color: 'red'
+  }
 });
+//react js의 css와 유사하다.
