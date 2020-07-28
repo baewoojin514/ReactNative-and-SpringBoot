8e1e8a17a167099d2e501a9257f26075a757e246
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-28 19:42:49 +0900

    scroll view is created

--- App.js
+++ App.js
@@ -1,64 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
-
-/*class CounterButton extends React.Component {
-  static defaultProps = {
-    counter: -1,
-  };
-  constructor(props){
-    super(props);
-
-    this.state = {
-      counter: props.counter,
-    };
-  }
-  clickHandler = () => {
-    this.setState({
-      counter: this.state.counter +1
-    })
-  }
-
-  render(){
-    return (
-      <View style={styles.buttonContainer}>
-        <Text>{this.state.counter}</Text>
-        <Button
-          title={"Click Me"}
-          onPress={this.clickHandler}
-        />
-      </View>
-    );
-
-  }
-
-
-}*/
+import { StyleSheet, Image, scrollView, Text, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 export default class App extends React.Component {
-  render(){
+  render() {
     return (
-      <View style={styles.container}>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }}/>
-        <StatusBar style="auto"/>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        </ScrollView>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     );
-
   }
-
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 2
-  }
+  },
 });
 //react js의 css와 유사하다.
