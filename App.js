ed4940ed5324a239c7ff49217d610b77adf32395
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-28 18:38:42 +0900

    several buttons are created with start count

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
 
 class CounterButton extends React.Component {
+  static defaultProps = {
+    counter: -1,
+  };
   constructor(props){
     super(props);
 
@@ -18,13 +21,12 @@ class CounterButton extends React.Component {
 
   render(){
     return (
-      <View>
+      <View style={styles.container}>
         <Text>{this.state.counter}</Text>
         <Button
-          title={"Click me!"}
+          title={"Click Me"}
           onPress={this.clickHandler}
         />
-        <StatusBar style="auto" />
       </View>
     );
 
@@ -37,14 +39,16 @@ export default class App extends React.Component {
   render(){
     return (
       <View style={styles.container}>
-        <CounterButton counter={1}/>
+        <CounterButton counter={0}/>
+        <CounterButton/>
+        <CounterButton counter={2}/>
+        <CounterButton counter={3}/>
       </View>
     );
 
   }
 
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
