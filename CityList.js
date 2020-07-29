dacb3f50674e2099191222d563c4df5df0a285df
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-29 15:05:57 +0900

    weather detail screen for specific city is created

--- CityList.js
+++ CityList.js
@@ -20,7 +20,9 @@ export default class CityList extends React.Component {
     }
     onPressCity(item) {
         console.log('onPressCity =', item);
-        this.props.navigation.navigate('Detail')
+        this.props.navigation.navigate('Detail', {
+            city: item
+        })
     }
     renderItem(city) {
         return (
